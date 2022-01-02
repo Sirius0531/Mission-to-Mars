b92743fc947ebce4c74d8caefced038229eec63b
Sirius <sirius6008@gmail.com>
2022-01-02 00:14:07 -0800

    week10

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 @app.route("/")
 def index():
    mars = mongo.db.mars.find_one()
-   return render_template("index2.html", mars=mars)
+   return render_template("indexadd.html", mars=mars)
 
 @app.route("/scrape")
 def scrape():
